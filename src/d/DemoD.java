46818bf0f0424a5cc7ac10e470dbd8eb27be8490
araslan <araslan@localhost>
2024-01-03 20:14:19 -0600

    Initial Commit

---
+++ src/d/DemoD.java
@@ -0,0 +1,15 @@
+package d;
+
+public class DemoD {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Thursday");
+		
+		System.out.println("I did it");
+		System.out.println("It was easy");
+
+	}
+
+
+}
